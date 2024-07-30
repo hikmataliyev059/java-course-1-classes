b18f25d8cebfa2e1b2b00535122109c00ef7c318
hikmet059 <javahikmet5909@gmail.com>
2024-07-30 11:01:59 +0400

    string processing task

---
+++ src/main/java/az/classes/MixedTasks/StringProcessing.java
@@ -0,0 +1,15 @@
+package az.classes.MixedTasks;
+
+public class StringProcessing {
+
+    public static void main(String[] args) {
+        String s1 = "salam";
+        final String s2 = "sa";
+        String s3 = "lam";
+        String s4 = "salam";
+
+        System.out.println(s1 == s4);
+        System.out.println(s1 == s2 + s3);
+        System.out.println(s1 == s2 + "lam");
+    }
+}
