1a8d1275a3297bebffdeefb1f7edd6df968fdeb1
hikmet059 <javahikmet5909@gmail.com>
2024-08-18 22:07:01 +0400

    enum task

---
+++ src/main/java/az/classes/Enumation/Season.java
@@ -0,0 +1,8 @@
+package az.classes.Enumation;
+
+public final class Season {
+    public static final String SPRING = "SPRING";
+    public static final String SUMMER = "SUMMER";
+    public static final String FALL = "FALL";
+    public static final String WINTER = "WINTER";
+}
