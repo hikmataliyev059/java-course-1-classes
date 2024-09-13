ba34e2054b55b3909b3f78a660dfbe8b24b99fb7
hikmet059 <javahikmet5909@gmail.com>
2024-09-13 19:28:28 +0400

    diamond proplem task

---
+++ src/main/java/az/classes/Interface/DiamondProplem/NatureSoundsApp.java
@@ -0,0 +1,10 @@
+package az.classes.Interface.DiamondProplem;
+
+public class NatureSoundsApp {
+
+    public static void main(String[] args) {
+        Parrot parrot = new Parrot();
+        parrot.sound();
+    }
+
+}
